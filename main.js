9116d19a9976c791371d0ba1e99f096817381007
Wate02 <alx.schule@gmail.com>
2024-04-18 18:53:07 +0200

    update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+
 console.log('Hello World!');
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+function func1() {
+  location.href = 'https://wate02.github.io/games';
+}
