3c2dd26068a483a892eaf05e7075c2ea07720a05
Wate02 <alx.schule@gmail.com>
2024-08-22 10:43:13 +0200

    update

--- main.js
+++ main.js
@@ -21,3 +21,14 @@ function func2() {
 function about() {
   location.href = 'https://wate02.github.io/about';
 }
+
+//<div id="yellowinfocard"><img id="iicon"src="/img/i.png" height="20px"> wate02.github.io kann jetzt als Web-app auf IOS verwendet werden. Clicke dafür auf <img id="shareicon"src="/img/share.png" height="20px" width="15px"> und dann auf "Zum Startbildschirm hinzufügen".
+//<style> 
+//#shareicon {
+//-webkit-filter: invert(1);
+//  filter: invert(1);}
+  
+//  #iicon {
+//  margin-bottom: -4px;
+//}
+//  </style></div>
